e7837800225cd410b2da8eb88512a97f7879a13a
Greg Sterling <gregory.sterling@me.com>
2025-08-26 08:24:56 -0400

    just a quick description addition to the script

--- firmware/get_firmware.sh
+++ firmware/get_firmware.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# A quick and dirty script to just download the firmware so that it doesn't need to be included in the repo.
 
 wget https://static.roland.com/assets/media/zip/sp404mk2_sys_v501.zip
 unzip -u sp404mk2_sys_v501.zip
